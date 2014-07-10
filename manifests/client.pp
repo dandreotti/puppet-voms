ad0b1c153e2f6b82f085a3d944e3b1797026001c
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-07-10 13:59:11 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -44,7 +44,7 @@ define puppet-voms::client ($vo = $name, $servers = []  ) {
                    group   => root,
                    mode    => 0755, 
                    recurse => true,
-                   purge   => true
+                   purge   => true,
                    require => File['/etc/grid-security/vomsdir']
   }
 
